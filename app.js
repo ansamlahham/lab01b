a710d833ae77e603026d7c3ee14aaac814e30dd5
ansamlahham <ansamlahham@outlook.com>
2021-04-25 14:37:30 +0300

    lab01b

---
+++ app.js
@@ -0,0 +1,14 @@
+let userName = prompt('What is your name?');
+console.log(userName);
+
+let userAge = prompt('What is your age?')
+console.log(userAge);
+
+let userHeight = prompt('How tall are you?')
+console.log(userHeight);
+
+let userWeight = prompt('How much do you weigh?')
+console.log(userWeight);
+
+alert (' Hello ' + userName + ' With ' + userAge + ' years old ' + ' Your Height is ' + userHeight + ' And your weight is ' + userWeight);
+
